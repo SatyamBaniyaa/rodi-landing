4e2debb885bd70f602e05353b02c1d64ee773c20
karanbasnet-KB <karanbasnet7138@gmail.com>
2026-05-07 12:31:46 +0545

    minor changes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <CommonNavbar />
   <v-main>
+<<<<<<< HEAD
     <!-- <Services /> -->
+=======
+    <Services />
+    <Contact/>
+>>>>>>> e976cf7 (minor changes)
   </v-main>
 </template>

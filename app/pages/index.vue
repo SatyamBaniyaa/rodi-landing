20baf966c13dc5f4704d1d5141eda214c56d5670
karanbasnet-KB <karanbasnet7138@gmail.com>
2026-05-07 12:40:20 +0545

    simple fixes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,7 @@
 <template>
   <CommonNavbar />
   <v-main>
-<<<<<<< HEAD
-    <!-- <Services /> -->
-=======
     <Services />
     <Contact/>
->>>>>>> e976cf7 (minor changes)
   </v-main>
 </template>
